ce8dad503d428784d8f622869ca62e5d7670a77e
bubungh0st <toufique.mallick@gmail.com>
2024-01-11 01:04:19 +1300

    About Section v1.1

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -150,4 +150,21 @@
     });
   }
 
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.abt-skill');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
 })()
